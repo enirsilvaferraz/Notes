7f6a8b811d2a1118a53f03abf510007d1df8caa7
Enir Francisco Silva Ferraz <enir.ferraz@bancointer.com.br>
2021-09-09 16:27:17 -0300

    [Compose] Melhorias na tela

--- app/src/main/java/com/ferraz/notes/views/MockHelper.kt
+++ app/src/main/java/com/ferraz/notes/views/MockHelper.kt
@@ -5,33 +5,10 @@ import com.ferraz.notes.database.NotesEntity
 object MockHelper {
 
     val items = listOf(
-        NotesEntity(uid = 1, title = "Title", description = "Comprar itens para lavar roupa"),
-        NotesEntity(uid = 2, description = "Fazer testes na Main Activity"),
-        NotesEntity(uid = 3, title = "Outro titulo"),
-        NotesEntity(uid = 4, description = "Não deixar de ir na academia essa semana"),
-
-        NotesEntity(uid = 1, description = "dnsoiun fsnoef f siefnsoien fesinfsoinienfoesin foi sefosiefnisien fosi enfosienso efinseoifns ktrdxhj"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "Teste"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "dnsoiun fsnoef f siefnsoien fesinfsoinienfoesin foi sefosiefnisien fosi enfosienso efinseoifns ktrdxhj"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "Teste"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "dnsoiun fsnoef f siefnsoien fesinfsoinienfoesin foi sefosiefnisien fosi enfosienso efinseoifns ktrdxhj"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "Teste"),
-
-        NotesEntity(uid = 1, description = "dnsoiun fsnoef f siefnsoien fesinfsoinienfoesin foi sefosiefnisien fosi enfosienso efinseoifns ktrdxhj"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "Teste"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "dnsoiun fsnoef f siefnsoien fesinfsoinienfoesin foi sefosiefnisien fosi enfosienso efinseoifns ktrdxhj"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "Teste"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "dnsoiun fsnoef f siefnsoien fesinfsoinienfoesin foi sefosiefnisien fosi enfosienso efinseoifns ktrdxhj"),
-        NotesEntity(uid = 1, description = "lsdam alsdlasmd lamsdlasmdlsam lamdlasdlsamdal lmdalfnberuibeg"),
-        NotesEntity(uid = 1, description = "Teste"),
+        NotesEntity(uid = 1, title = "Card só com titulo"),
+        NotesEntity(uid = 2, description = "Card só com descrição"),
+        NotesEntity(uid = 3, title = "Card com titulo e descrição", description = "Card com titulo e descrição"),
+        NotesEntity(uid = 4, description = "Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa."),
+        NotesEntity(uid = 5, title= "Card com título e descrição longa", description = "Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa. Card com descrição longa."),
     )
 }
--- app/src/main/java/com/ferraz/notes/views/ui/components/NoteCard.kt
+++ app/src/main/java/com/ferraz/notes/views/ui/components/NoteCard.kt
@@ -4,14 +4,14 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,8 +35,7 @@ fun NoteCard(
         elevation = 4.dp,
         modifier = Modifier
             .padding(4.dp)
-            .height(148.dp)
-            .width(344.dp)
+            .height(150.dp)
             .combinedClickable(
                 onClick = {
                     onClick?.invoke(note)
@@ -48,9 +47,7 @@ fun NoteCard(
     ) {
 
         ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp)
+            modifier = Modifier.fillMaxSize()
         ) {
 
             val (header, subtitle) = createRefs()
@@ -60,13 +57,15 @@ fun NoteCard(
                     text = note.title.orEmpty(),
                     style = Typography.h6,
                     overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold,
                     maxLines = 1,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .constrainAs(header) {
-                            top.linkTo(parent.top)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
+                    modifier = Modifier.constrainAs(header) {
+                            top.linkTo(parent.top, 16.dp)
+                            start.linkTo(parent.start, 16.dp)
+                            end.linkTo(parent.end, 16.dp)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.wrapContent
+
                         }
                 )
 
@@ -75,14 +74,16 @@ fun NoteCard(
                     text = note.description.orEmpty(),
                     style = Typography.body1,
                     overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier
-                        .constrainAs(subtitle) {
-                            top.linkTo(
-                                if (!note.title.isNullOrBlank()) header.bottom else parent.top
-                            )
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                            bottom.linkTo(parent.bottom)
+                    maxLines = if (!note.title.isNullOrBlank()) 5 else 7,
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.constrainAs(subtitle) {
+
+                            if (!note.title.isNullOrBlank()) top.linkTo(header.bottom, 6.dp)
+                            else top.linkTo(parent.top, 16.dp)
+
+                            start.linkTo(parent.start, 16.dp)
+                            end.linkTo(parent.end, 16.dp)
+                            bottom.linkTo(parent.bottom, 8.dp)
                             width = Dimension.fillToConstraints
                             height = Dimension.fillToConstraints
                         }
@@ -99,5 +100,7 @@ fun PreviewNoteCard() {
         NoteCard(MockHelper.items[0])
         NoteCard(MockHelper.items[1])
         NoteCard(MockHelper.items[2])
+        NoteCard(MockHelper.items[3])
+        NoteCard(MockHelper.items[4])
     }
 }
